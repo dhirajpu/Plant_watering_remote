d7dc37acbbde179f0b741941c2daa525c25aad52
dhirajpu <38656034+dhirajpu@users.noreply.github.com>
2026-08-29 14:27:10 +0530

    Add V2 dashboard config

---
+++ v2/config.js
@@ -0,0 +1,7 @@
+window.PLANT_V2_CONFIG = {
+  firebaseBaseUrl: "https://vernal-catfish-196407.firebaseio.com",
+  deviceRoot: "/plantMonitor/v2",
+  authToken: "",
+  staleTimeoutSec: 15,
+  refreshMs: 3000
+};
